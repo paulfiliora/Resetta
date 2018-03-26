b666c93c5a013f4d1d38d0d0df31d9ef95844390
Paul Filiora <paul.filiora@gmail.com>
2018-03-26 10:55:04 -0400

    recieving results json from API

---
+++ tunr-ui/src/components/SearchBar.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { Icon, Input, Form } from 'semantic-ui-react'
+import axios from 'axios'
+
+export class SearchBar extends Component{
+
+  state = {
+    base_url:'https://api.edamam.com/search?q=',
+    query: '',
+    app_id:'&app_id=ef03f549',
+    api_key:'&app_key=a9fe5f06c5a1c078ffc893b930a74fea',
+    calories: 'calories=591-722',
+    health:'health=alcohol-free',
+    results: []
+}
+
+
+handleChange = (event) => {
+  const queryValue = event.target.value;
+  this.setState({ query: queryValue })
+};
+
+handleSubmit = (event) => {
+  event.preventDefault()  
+  this.queryURL()
+};
+
+async queryURL () {
+  try{
+    const response = await axios.get(
+      this.state.base_url
+      +this.state.query
+      +this.state.app_id
+      +this.state.api_key
+    )
+    this.setState({ results: response.data.hits })
+    console.log(this.state.results)
+  }
+  catch(error) {
+    console.log('Error retrieving query!')
+    console.log(error);
+  }
+};
+
+  render(){
+    
+    return(
+      <Form onSubmit={this.handleSubmit}>
+      <Input
+        icon={<Icon name='search' inverted circular link />}
+        placeholder='Find a recipe...'
+        onChange={this.handleChange}
+      />
+      </Form>
+    )
+  }
+}
+
+export default SearchBar
---
+++ tunr-ui/src/container/Homepage.js
@@ -0,0 +1,231 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import {
+  Button,
+  Container,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react'
+import SearchBar from '../components/SearchBar'
+
+const HomepageHeading = ({ mobile }) => (
+  <Container text>
+    <Header
+      as='h1'
+      content='Resetta'
+      inverted
+      style={{
+        fontSize: mobile ? '2em' : '4em',
+        fontWeight: 'normal',
+        marginBottom: 0,
+        marginTop: mobile ? '1.5em' : '3em',
+      }}
+    />
+    <SearchBar/>
+  </Container>
+)
+
+HomepageHeading.propTypes = {
+  mobile: PropTypes.bool,
+}
+
+/* 
+ * Custom Responsive containers
+ */
+class DesktopContainer extends Component {
+  state = {}
+
+  hideFixedMenu = () => this.setState({ fixed: false })
+  showFixedMenu = () => this.setState({ fixed: true })
+
+  render() {
+    const { children } = this.props
+    const { fixed } = this.state
+
+    return (
+      <Responsive {...Responsive.onlyComputer}>
+        <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
+          <Segment className='banner' inverted textAlign='center' style={{ minHeight: 700, padding: '1em 0em' }} vertical>
+            <Menu
+              fixed={fixed ? 'top' : null}
+              inverted={!fixed}
+              pointing={!fixed}
+              secondary={!fixed}
+              size='large'
+            >
+              <Container>
+                <Menu.Item as='a' active>Home</Menu.Item>
+                <Menu.Item as='a'>Collections</Menu.Item>
+                <Menu.Item position='right'>
+                  <Button as='a' inverted={!fixed}>Log in</Button>
+                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                </Menu.Item>
+              </Container>
+            </Menu>
+            <HomepageHeading />
+          </Segment>
+        </Visibility>
+
+        {children}
+      </Responsive>
+    )
+  }
+}
+
+DesktopContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+class MobileContainer extends Component {
+  state = {}
+
+  handlePusherClick = () => {
+    const { sidebarOpened } = this.state
+
+    if (sidebarOpened) this.setState({ sidebarOpened: false })
+  }
+
+  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
+
+  render() {
+    const { children } = this.props
+    const { sidebarOpened } = this.state
+
+    return (
+      <Responsive {...Responsive.onlyMobile}>
+        <Sidebar.Pushable>
+          <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
+            <Menu.Item as='a' active>Home</Menu.Item>
+            <Menu.Item as='a'>Collections</Menu.Item>
+            <Menu.Item as='a'>Log in</Menu.Item>
+            <Menu.Item as='a'>Sign Up</Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
+            <Segment className="banner" inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em' }} vertical>
+              <Container>
+                <Menu inverted pointing secondary size='large'>
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name='sidebar' />
+                  </Menu.Item>
+                  <Menu.Item position='right'>
+                    <Button as='a' inverted>Log in</Button>
+                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                  </Menu.Item>
+                </Menu>
+              </Container>
+              <HomepageHeading mobile />
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Responsive>
+    )
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const ResponsiveContainer = ({ children }) => (
+  <div>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </div>
+)
+
+ResponsiveContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const Homepage = () => (
+  <ResponsiveContainer>
+    <Segment style={{ padding: '8em 0em' }} vertical>
+    <Grid container stackable verticalAlign='middle'>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header as='h2' style={{ fontSize: '2em' }}>Recipe Search</Header>
+            <p style={{ fontSize: '1.33em' }}>
+            1.7+ million nutritionally analyzed recipes <br/>>
+            Detailed macro and micronutrient data<br/>>
+            Filter by calories, diets and allergens
+            </p>
+            <Header as='h2' style={{ fontSize: '2em' }}>Nutrition Analysis</Header>
+            <p style={{ fontSize: '1.33em' }}>
+            Analyze any recipe or ingredient list<br/>>
+            Automatic tagging for allergens and popular diets<br/>>
+            Database of 540,000+ food items
+            </p>
+          </Grid.Column>
+          <Grid.Column floated='right' width={6}>
+            <Image
+              bordered
+              rounded
+              size='large'
+              src='https://static.pexels.com/photos/277253/pexels-photo-277253.jpeg?cs=srgb&dl=anise-aroma-art-277253.jpg&fm=jpg'
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+
+    <Segment style={{ padding: '0em' }} vertical>
+      <Grid celled='internally' columns='equal' stackable>
+        <Grid.Row textAlign='center'>
+          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <Header as='h3' style={{ fontSize: '2em' }}>"What a time-saver!"</Header>
+            <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+          </Grid.Column>
+          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <Header as='h3' style={{ fontSize: '2em' }}>"I can be as precise with meal prep as I need."</Header>
+            <p style={{ fontSize: '1.33em' }}>
+              NY Sports Club Body builder
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+
+
+    <Segment inverted vertical style={{ padding: '5em 0em' }}>
+      <Container>
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='About' />
+              <List link inverted>
+                <List.Item as='a'>Sitemap</List.Item>
+                <List.Item as='a'>Contact Us</List.Item>
+                <List.Item as='a'>API source</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Services' />
+              <List link inverted>
+                <List.Item as='a'>Recipe Searchr</List.Item>
+                <List.Item as='a'>Nutrion Analysis</List.Item>
+                <List.Item as='a'>Favorites Collection</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header as='h4' inverted>Footer Header</Header>
+              <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Segment>
+  </ResponsiveContainer>
+)
+
+export default Homepage
